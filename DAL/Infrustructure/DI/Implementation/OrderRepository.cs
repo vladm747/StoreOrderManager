4ff1fd057633dbf78239e951bafea6834d82c8e1
vladm747 <vladyslav.moroz105@gmail.com>
2023-06-27 23:35:20 +0300

    add order repository

---
+++ DAL/Infrustructure/DI/Implementation/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Context;
+using DAL.Entities;
+using DAL.Infrustructure.DI.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Infrustructure.DI.Implementation
+{
+    public class OrderRepository: RepositoryBase<Order, int>, IOrderRepository
+    {
+        public OrderRepository(NorthwindContext context) : base(context) { }
+    }
+}
